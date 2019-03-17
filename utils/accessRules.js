beb587a250a21fefd6cbfbcbafffa495191251d6
Artur Chubukliiev <artur.it2914@gmail.com>
2019-03-17 22:11:57 +0200

    home work 2

--- utils/accessRules.js
+++ utils/accessRules.js
@@ -20,30 +20,28 @@ class CrudRule {
     }
 }
 
-const role_CRUD_rules = new Map([
-    [
-        ADMIN, new CrudRule(
+const role_CRUD_rules = new Map();
+role_CRUD_rules.set(
+    ADMIN, new CrudRule(
         new ActionRule(false, [ADMIN, MODERATOR, USER]),
         new ActionRule(true, [ADMIN, MODERATOR, USER]),
         new ActionRule(true, [ADMIN, MODERATOR, USER]),
         new ActionRule(false, [ADMIN, MODERATOR, USER])
-    )],
-    [
-        MODERATOR, new CrudRule(
-        new ActionRule(false, [MODERATOR, USER]),
-        new ActionRule(true, [MODERATOR, USER]),
-        new ActionRule(true, [MODERATOR, USER]),
-        new ActionRule(true, []),
-    )],
-    [
-        USER, new CrudRule(
-        new ActionRule(true, []),
-        new ActionRule(true, []),
-        new ActionRule(true, []),
-        new ActionRule(true, []),
     )
-    ]
-]);
+)
+    .set(
+        MODERATOR, new CrudRule(
+            new ActionRule(false, [MODERATOR, USER]),
+            new ActionRule(true, [MODERATOR, USER]),
+            new ActionRule(true, [MODERATOR, USER]),
+            new ActionRule(true, [])
+        ))
+    .set(
+        USER, new CrudRule(
+            new ActionRule(true, []),
+            new ActionRule(true, []),
+            new ActionRule(true, []),
+            new ActionRule(true, [])));
 
 
 module.exports.check_role_access = (actor, obj, actionName) => {
@@ -57,25 +55,21 @@ module.exports.check_role_access = (actor, obj, actionName) => {
     let isAllowed = false;
     const itSelf = actor._id === obj._id;
 
-    for (let i = 0; i < actor_roles.length; i++) {
-        let actionRule=role_CRUD_rules[actor_roles[i]].rules[actionName];
-        if (itSelf) {
-           isAllowed= actionRule.self;
-           if(isAllowed)
-               break;
-        } else {
-            let supervisedRoles = actionRule.other;
-            for (let j = 0; j < obj_roles.length; j++) {
-                isAllowed = supervisedRoles.indexOf(obj_roles[j]) !== -1;
+    if (itSelf) {
+        for (let actorRole of actor_roles) {
+            if (role_CRUD_rules.get(actorRole).rules.get(actionName).self)
+                return true;
+        }
+    } else {
+        for (let actorRole of actor_roles) {
+            const subordinate_roles = role_CRUD_rules.get(actorRole).rules.get(actionName).other;
+            for (let objRole of obj_roles) {
+                isAllowed = subordinate_roles.indexOf(objRole) !== -1;
                 if (!isAllowed)
                     break;
             }
         }
-
-
     }
-
-
     return isAllowed;
 };
 
